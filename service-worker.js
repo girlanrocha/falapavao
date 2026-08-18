4034debf332059f9d7ed054077f6bd055558f4ed
girlanrocha <girlansrocha@gmail.com>
2026-08-18 19:01:28 -0300

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,8 +1,9 @@
-const CACHE_NAME = "fala-pavao-v1";
+const CACHE_NAME = "fala-pavao-v2";
+
 const APP_SHELL = [
   "./",
   "./index.html",
-  "./style.css",
+  "./estilo.css",
   "./app.js",
   "./manifest.json",
   "./icons/icon-192.png",
@@ -14,6 +15,7 @@ self.addEventListener("install", (event) => {
   event.waitUntil(
     caches.open(CACHE_NAME).then((cache) => cache.addAll(APP_SHELL))
   );
+
   self.skipWaiting();
 });
 
@@ -21,27 +23,32 @@ self.addEventListener("activate", (event) => {
   event.waitUntil(
     caches.keys().then((keys) =>
       Promise.all(
-        keys.filter((key) => key !== CACHE_NAME).map((key) => caches.delete(key))
+        keys
+          .filter((key) => key !== CACHE_NAME)
+          .map((key) => caches.delete(key))
       )
     )
   );
+
   self.clients.claim();
 });
 
 self.addEventListener("fetch", (event) => {
   if (event.request.method !== "GET") return;
+
   event.respondWith(
-    caches.match(event.request).then((cached) => {
-      const network = fetch(event.request)
-        .then((response) => {
-          if (response.ok) {
-            const clone = response.clone();
-            caches.open(CACHE_NAME).then((cache) => cache.put(event.request, clone));
-          }
-          return response;
-        })
-        .catch(() => cached);
-      return cached || network;
-    })
+    fetch(event.request)
+      .then((response) => {
+        if (response && response.ok) {
+          const clone = response.clone();
+
+          caches.open(CACHE_NAME).then((cache) => {
+            cache.put(event.request, clone);
+          });
+        }
+
+        return response;
+      })
+      .catch(() => caches.match(event.request))
   );
 });
